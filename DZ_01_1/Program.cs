372bc8141f0cfcba639b1e206c702248e5513bfc
Tim F <DEVILkin666@yandex.ru>
2023-05-24 19:55:07 +0600

    DZ 01 C# task 1-4

---
+++ DZ_01_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Ведите числа А и В");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int Max = 0;
+int Min = 0;
+if(A > B)
+{    
+    Max = A;
+    Min = B;
+}
+else
+{
+    Max = B;
+    Min = A;
+}
+Console.WriteLine("Max =" + Max);
+Console.WriteLine("Min =" + Min);
---
+++ DZ_01_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Ведите числа А и В");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int Max = 0;
+if(A > B && A > C)
+{    
+    Max = A;
+    }
+else if (B > A && B > C)
+{
+    Max = B;
+    }
+else
+{
+    Max = C;
+    }
+Console.WriteLine("Max =" + Max);
