b300062d54de0e62cf5882e3633b479aa332aea9
Tim F <DEVILkin666@yandex.ru>
2023-05-24 21:05:08 +0600

    DZ 01 C# update task2

--- DZ_01_2/Program.cs
+++ DZ_01_2/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Ведите числа А и В");
+Console.WriteLine("Ведите числа А,В и C");
 int A = int.Parse(Console.ReadLine());
 int B = int.Parse(Console.ReadLine());
 int C = int.Parse(Console.ReadLine());
