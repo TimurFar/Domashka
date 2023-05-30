a97c323bf6b642899155eb1a0e0794da9c224c2e
Tim F <DEVILkin666@yandex.ru>
2023-05-30 19:56:18 +0600

    DZ 2_1 second task done

--- DZ_02_1/Program.cs
+++ DZ_02_1/Program.cs
@@ -17,13 +17,36 @@
 // return secondNumber;
 // }
 
-Console.WriteLine("введите число от 100 до 999");
-int A = int.Parse(Console.ReadLine());
-if (A > 99 & A < 1000) 
+// Console.WriteLine("введите число от 100 до 999"); // первый вариант решения
+// int A = int.Parse(Console.ReadLine());
+// if (A > 99 & A < 1000) 
+// {
+//     Console.WriteLine((A%100)/10);
+// }
+// else
+// {
+//     Console.WriteLine("Вне диапазона");
+// }
+
+int FindSecondDigit(int number)  //второе решение
 {
-    Console.WriteLine((A%100)/10);
+    int secondDigit = (number / 10) % 10;
+    return secondDigit;
+}
+
+void FindDigit(int number, int secondDigit)
+{
+    if(number > 99 && number < 1000)
+{
+    Console.WriteLine(secondDigit);
 }
 else
 {
     Console.WriteLine("Вне диапазона");
 }
+}
+
+Console.WriteLine("введите число от 100 до 999");
+int number = int.Parse(Console.ReadLine());
+int numberSecond = FindSecondDigit(number);
+FindDigit(number, numberSecond);
