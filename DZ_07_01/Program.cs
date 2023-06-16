83642ae526e865d95ddaab54e4ef9ab9857d3b6b
Tim F <DEVILkin666@yandex.ru>
2023-06-16 22:48:58 +0600

    DZ 07 task01

---
+++ DZ_07_01/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void PrintArray (double [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"  {matr[i,j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (double [,] matr)
+{
+     for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().NextDouble()*10;
+            
+        }
+        
+    }
+}
+Console.WriteLine("Введите количество строк");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество стобцов");
+int n = int.Parse(Console.ReadLine());
+double [,] matrix = new double [m,n];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
