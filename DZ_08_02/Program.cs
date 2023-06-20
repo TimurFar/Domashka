048c238b36c1f9e21f61ffbbd8853ce7f8d1f1f5
Tim F <DEVILkin666@yandex.ru>
2023-06-20 22:49:42 +0600

    DZ 08 task01+2+3+

---
+++ DZ_08_02/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет 
+//находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
+// наименьшей суммой элементов: 1 строка
+
+void PrintArray (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"  {matr[i,j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int [,] matr)
+{
+     for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+            
+        }
+        
+    }
+}
+
+double [] GetResultAarray (int [,] array)
+{
+    double [] result = new double[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        double sum = 0; 
+        for (int j =0; j < array.GetLength(1); j++)
+        {
+            sum += array[i,j];
+        }
+        result [i] = Math.Round(sum, 2); 
+        
+    }
+    return result;
+}
+
+void  MinSum(double [] matrix)
+{
+    double min = matrix [0];
+    double number = 0;
+        for (int i =0; i < matrix.Length; i++)
+        {
+            if (min > matrix [i])
+        {
+            min = matrix [i];
+            number = i;
+        }
+                    // Console.WriteLine(matrix [i]);
+               }
+         Console.WriteLine($"Значение min равно {min} ");
+        Console.WriteLine($"Строка с наименьшей суммой иммет номер = {number}");
+}
+
+Console.WriteLine("Введите количество строк");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество стобцов");
+int n = int.Parse(Console.ReadLine()!);
+int [,] matrix = new int [m,n];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+double [] AllSum = GetResultAarray(matrix);
+Console.WriteLine($"Сумма каждой строки = {String.Join("; ", AllSum)}");
+Console.WriteLine();
+double [] sums = GetResultAarray(matrix);
+MinSum(sums);
+
+
+
+
+
+
+
---
+++ DZ_08_03/Program.cs
@@ -0,0 +1,88 @@
+﻿// // Задача 58: Задайте две матрицы. Напишите программу, которая будет 
+// // находить произведение двух матриц.
+// // Например, даны 2 матрицы:
+// // 2 4 | 3 4
+// // 3 2 | 3 3
+// // Результирующая матрица будет:
+// // 18 20
+// // 15 18
+
+
+void PrintArray (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"  {matr[i,j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int [,] matr)
+{
+     for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+            
+        }
+        
+    }
+}
+
+          // метод для умножения матриц
+ int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+    {       
+        if (matrixA.GetLength(1) != matrixB.GetLength(0))
+        {
+            Console.WriteLine("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+        }
+
+        var matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+        for (int i = 0; i < matrixA.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixB.GetLength(1); j++)
+            {
+                matrixC[i, j] = 0;
+
+                for (int k = 0; k < matrixA.GetLength(0); k++)
+                {
+                    matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                }
+            }
+        }
+
+        return matrixC;
+    }
+
+
+
+
+Console.WriteLine("Введите количество строк первой матрицы");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество стобцов первой матрицы");
+int n = int.Parse(Console.ReadLine()!);
+int [,] matrix = new int [m,n];
+FillArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Первая матрица: ");
+PrintArray(matrix);
+
+Console.WriteLine("Введите количество строк второй матрицы");
+int m1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество стобцов второй матрицы");
+int n1 = int.Parse(Console.ReadLine()!);
+int [,] matrix1 = new int [m1,n1];
+FillArray(matrix1);
+Console.WriteLine();
+Console.WriteLine("Вторая матрица: ");
+PrintArray(matrix1);
+Console.WriteLine();
+
+Console.WriteLine("Произведение двух матриц равно: ");
+PrintArray(MatrixMultiplication(matrix, matrix1));
+
